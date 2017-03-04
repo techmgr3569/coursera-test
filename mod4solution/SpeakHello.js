46cf39ec9ec9b3eb27fa01fc2c35b0b43db36efe
Kenny Alvarez <ken.alvarez@hotmail.com>
2017-03-04 01:04:48 -0500

    My Mod 4 Solution. KA

--- mod4solution/SpeakHello.js
+++ mod4solution/SpeakHello.js
@@ -1,24 +1,27 @@
-// STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
-// See Lecture 52, part 2
+(function	(window) {                  //adding '(window)' allows the IIFE to bypass the speakWord var yet-
+	var helloSpeaker = {};				//still create the object 'helloSpeaker' and add it to GlobalScope.
 
 
-// STEP 3: Create an object, called 'helloSpeaker' to which you will attach
-// the "speak" method and which you will expose to the global context
-// See Lecture 52, part 1
-// var helloSpeaker =
-
-// DO NOT attach the speakWord variable to the 'helloSpeaker' object.
 var speakWord = "Hello";
+
 
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
-// See Lecture 52, part 2
-function speak(name) {
-  console.log(speakWord + " " + name);
+
+helloSpeaker.speak = function (name) {	//this attaches the helloSpeaker obj. to 'speak' function 
+ console.log(speakWord + " " + name);	//outputs to console
 }
+
+window.helloSpeaker = helloSpeaker;     //this is the connect point for '(window)' operate to allow it -
+										//the helloSpeaker obj. to be added to global scope at this point
+})(window);								//this immediately invokes the (window) for 'helloSpeaker' function
 
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
 // 'helloSpeaker' on the global scope as well.
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 // xxxx.xxxx = helloSpeaker;
+
+
+// STEP 3: object 'helloSpeaker' ~ attach "speak" method and  which you will expose to the global context
+// See Lecture 52, part 1
